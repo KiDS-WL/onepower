32d4f14098774852e5d84be8e5d3d5943aef769c
maricool <ma@roe.ac.uk>
2022-09-27 16:44:35 +0100

    initial clean up done

--- load_utilities.py
+++ load_utilities.py
@@ -6,8 +6,10 @@ from scipy import interp
 
 from scipy.integrate import quad, simps, trapz
 
+
+# Marika: remove this and move to the main code
 def get_linear_power_spectrum(block, z_vec):
-    # AD: growth factor should be computed from camb/hmf directly, in thin way we can load Plin directly without this functions!
+    # AD: growth factor should be computed from camb/hmf directly, this way we can load Plin directly without this functions!
 	k_vec = block['matter_power_lin', 'k_h']
 	z_pl = block['matter_power_lin', 'z']
 	matter_power_lin = block['matter_power_lin', 'p_k']
