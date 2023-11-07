13c90dc0e32053a6318e8e4a216f6552e4b96833
maricool <ma@roe.ac.uk>
2023-11-07 17:02:33 +0000

    changed the name of extrapolate_obs to predict_observable. This is because the main functionality of this module is to create the prediction for the observable, for example stellar mass function versus stellar mass.

--- predict_observable.py
+++ predict_observable.py
@@ -1,8 +1,8 @@
 """
 This module combines tomographic / stellar mass bins of the individually calculated observables
+It produces the theoretical prediction for the observable for the full survey. 
+The number of bins and the mass range can be different to what is calculated in the hod_interface.py module.
 """
-
-# TODO: Add comment about the module. Variable names need to change to make it readable. 
 
 from cosmosis.datablock import names, option_section
 import numpy as np
@@ -36,13 +36,25 @@ def load_kernel(block, kernel_section, bin, z_ext, extrapolate_option):
     return kernel_ext
 
 
+# ;extrapolates the observable (e.g. stellar mass function) for 
+# [extrapolate_obs]
+# file = %(HM_PATH)s/extrapolate_obs.py
+# input_section_name  = stellar_mass_function
+# output_section_name = smf_red
+# ;suffixes = 1 2
+# sample  = nz_lens
+# obs_min = 9.3 ;10.4
+# obs_max = 11.3 ;10.1 
+# n_obs = 12 ;6
+# ; Stellar mass ranges
 def setup(options):
 	
     config = {}
-        
-    config['input_section_name']  = options.get_string(option_section, 'input_section_name',default='stellar_mass_function')
+    # input and output section names
+    config['input_section_name']  = options.get_string(option_section, 'input_section_name',  default='stellar_mass_function')
     config['output_section_name'] = options.get_string(option_section, 'output_section_name', default='obs_out')
     
+    # check if suffixes exists in the extrapolate_obs section of pipeline.ini
     if options.has_value(option_section, 'suffixes'):
         config['suffixes'] = options[option_section, 'suffixes']
         config['nbins']    = len(config['suffixes'])
@@ -60,9 +72,10 @@ def setup(options):
     if not np.all(np.array([len(config['obs_min']), len(config['obs_max']), len(config['n_obs'])]) == len(config['suffixes'])):
         raise Exception('Error: obs_min, obs_max, n_obs need to be of same length as the number of suffixes provided or equal to one.')
 
-    config['x_arr']   = []
+    # observable array
+    config['obs_arr']   = []
     for i in range(config['nbins']):
-        config['x_arr'].append(np.logspace(config['obs_min'][i], config['obs_max'][i], config['n_obs'][i]))
+        config['obs_arr'].append(np.logspace(config['obs_min'][i], config['obs_max'][i], config['n_obs'][i]))
     
     return config
 	
@@ -71,18 +84,18 @@ def execute(block, config):
 	
     input_section_name  = config['input_section_name']
     output_section_name = config['output_section_name']
-    x_arr    = config['x_arr']
+    obs_arr  = config['obs_arr']
     suffixes = config['suffixes']
     nbins    = config['nbins']
 
     # number of bins for the observable
     for i in range(nbins):
-        z_obs, obs_ext = load_and_extrapolate_obs(block, input_section_name, suffixes[i], x_arr[i], 0.0)
+        z_obs, obs_ext = load_and_extrapolate_obs(block, input_section_name, suffixes[i], obs_arr[i], 0.0)
     #                def load_and_extrapolate_obs(block, obs_section, suffix_in, x_ext, extrapolate_option):
-
+        print(z_obs)
         if z_obs is not None:
             # Load kernel if exists
-            print('in here')
+            print('z_obs is not None')
             nz = load_kernel(block, config['sample'], i+1, z_obs, 0.0)
             # Integrate over n(z)
             obs_out = simps(nz[:,np.newaxis]*obs_ext, z_obs, axis=0)
@@ -90,7 +103,7 @@ def execute(block, config):
             # Just use interpolated result at the median redshift for the output
             obs_out = obs_ext
         block.put_double_array_1d(output_section_name, 'bin_'+str(i+1), obs_out)
-        block.put_double_array_1d(output_section_name, 'obs_'+str(i+1), x_arr[i])
+        block.put_double_array_1d(output_section_name, 'obs_'+str(i+1), obs_arr[i])
     
     return 0
 
