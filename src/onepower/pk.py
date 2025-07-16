c6fa2d2e609dcc7e989824d861f2224e7a94dece
andrejdvornik <andrej.dvornik@me.com>
2025-07-16 07:16:38 +0000

    fix imports

--- src/onepower/pk.py
+++ src/onepower/pk.py
@@ -46,8 +46,8 @@ from hmf._internals._framework import Framework
 
 from .ia import SatelliteAlignment
 from .bnl import NonLinearBias
-from .hod import hod as hod_class
-from .hmi import HaloModelIngredients, HaloModelIngredientsNoLoop
+from .hmi import HaloModelIngredients
+from . import hod as hod_class
 
 valid_units = ['1/h', '1/h^2']
 
