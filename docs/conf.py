491350f92c5a99fb3169bdd4d1febd355a92cfeb
andrejdvornik <andrej.dvornik@me.com>
2025-08-14 08:57:59 +0000

    Update changelog, theme

--- docs/conf.py
+++ docs/conf.py
@@ -129,19 +129,22 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'pydata_sphinx_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-# html_theme_options = {}
+html_theme_options = {
+"navbar_start": ["navbar-logo"],
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-# html_title = None
+html_title = "OnePower"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 # html_short_title = None
