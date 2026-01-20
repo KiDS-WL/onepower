1fb64c05e94323c1a61b034b76ff7d45b448dba5
andrejdvornik <andrej.dvornik@me.com>
2026-01-20 12:08:54 +0000

    Remove plots

--- cosmosis_modules/legacy/likelihood_ds_wp/mini_2pt_like.py
+++ cosmosis_modules/legacy/likelihood_ds_wp/mini_2pt_like.py
@@ -221,15 +221,15 @@ def execute(block, config):
                                             mode=binning_mode,
                                             interpolated_x=data_x,
                                             bin_edges=edges[count])
-            pl.plot(data_x, data_vectors[count])
-            pl.plot(data_x, theory_vectors[count])
-            pl.plot(data_x, data_vectors[count]/theory_vectors[count])
+            #pl.plot(data_x, data_vectors[count])
+            #pl.plot(data_x, theory_vectors[count])
+            #pl.plot(data_x, data_vectors[count]/theory_vectors[count])
             count += 1
 
-        pl.xscale('log')
-        pl.yscale('log')
-        pl.show()
-        pl.clf
+        #pl.xscale('log')
+        #pl.yscale('log')
+        #pl.show()
+        #pl.clf
 
     residuals = data_vectors - theory_vectors
     chi2 = np.array([np.dot(residuals[m], np.dot(inv_cov[m][n], residuals[n]))
